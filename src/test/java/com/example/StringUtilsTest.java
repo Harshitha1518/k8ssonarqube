eba0b8aec7921ff1a25d67f76b4c358b440c791a
Harshitha1518 <devopslearning152018@gmail.com>
2026-01-28 16:50:10 +0530

    Add unit tests for StringUtils methods

---
+++ src/test/java/com/example/StringUtilsTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    StringUtils utils = new StringUtils();
+
+    @Test
+    void testReverse() {
+        assertEquals("olleh", utils.reverse("hello"));
+    }
+
+    @Test
+    void testPalindromeTrue() {
+        assertTrue(utils.isPalindrome("Madam"));
+    }
+
+    @Test
+    void testPalindromeFalse() {
+        assertFalse(utils.isPalindrome("Hello"));
+    }
+
+    @Test
+    void testNullInput() {
+        assertEquals("", utils.reverse(null));
+        assertFalse(utils.isPalindrome(null));
+    }
+}
